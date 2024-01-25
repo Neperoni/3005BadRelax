22ce85a2296f7480c3e281abfda1e261e9f6e944
Neperoni <90918860+Neperoni@users.noreply.github.com>
2024-01-24 22:06:14 -0500

    I think it works? Doesnt support ordered queries though

--- code_1.py
+++ code_1.py
@@ -55,7 +55,36 @@ def printDatabase(data):
         print(line)
 
 
+def filter_entries(database, labelIndex, condition, targetQuantity):
+    if condition == "!=":
+        # not equal
+        predicate = lambda entry: entry[labelIndex] != targetQuantity
+    elif condition == "<=":
+        # less than or equal
+        predicate = lambda entry: entry[labelIndex] <= targetQuantity
+    elif condition == ">=":
+        # greater than or equal
+        predicate = lambda entry: entry[labelIndex] >= targetQuantity
+    elif condition == "<":
+        # less than
+        predicate = lambda entry: entry[labelIndex] < targetQuantity
+    elif condition == ">":
+        # greater than
+        predicate = lambda entry: entry[labelIndex] > targetQuantity
+    elif condition == "=":
+        # equal
+        predicate = lambda entry: entry[labelIndex] == targetQuantity
+    else:
+        print("Invalid condition, please type one of the following: = <= < > >= !=")
+        return None
 
+    copyDatabase = database.copy()
+
+    # Apply the filter based on the predicate
+    result = list(filter(predicate, copyDatabase["entries"]))
+    copyDatabase["entries"] = result
+
+    return copyDatabase
 
 
 def select(args):
@@ -77,7 +106,7 @@ def select(args):
     if(not targetQuantity.isnumeric()):
         print("Invalid target quantity")
         return None
-    targetQuantity = int(targetQuantity)
+    targetQuantity = targetQuantity
     
     if(targetDatabase not in loadedDatabases):
         print("Target database not loaded")
@@ -95,92 +124,225 @@ def select(args):
     condition = "".join(sorted(condition))
     #so now the order doesnt matter
     
-    if(condition == "!="):
-        #not equal
-        predicate = lambda entry: (int(entry[labelIndex]) != targetQuantity)
-    elif(condition == "<="):
-        #less than or equal
-        predicate = lambda entry: (int(entry[labelIndex]) <= targetQuantity) 
-    elif(condition == "=>"):
-        #greather than or equal
-        predicate = lambda entry: (int(entry[labelIndex]) >= targetQuantity) 
-    elif(condition == "<"):
-        #less than
-        predicate = lambda entry: (int(entry[labelIndex]) < targetQuantity) 
-    elif(condition == ">"):
-        #greater than
-        predicate = lambda entry: (int(entry[labelIndex]) > targetQuantity) 
-    elif(condition == "="):
-        #equal
-        predicate = lambda entry: (int(entry[labelIndex]) == targetQuantity) 
-    else:
-        print("Invalid condition, please type one of the following: = <= < > >= !=")
-        return None
-    
-    copyDatabase = database.copy()
-    
-    result = list(filter(predicate, copyDatabase["entries"]))
-    copyDatabase["entries"] = result
-    
-    return copyDatabase
+    return filter_entries(database, labelIndex, condition, targetQuantity)
+
 
 def databaseSet(args):
-    pass
+    print(args)
+    #or(Employees, Managers)
+    #and(Employees, Managers)
+    #minus(Employees, Managers)
     
-def join(args):
-    pass
+    firstDatabaseIndex = indexOf(args, '(')+1
+
+    dataBases = args[firstDatabaseIndex:-1].split(",")
+    commandType = args[0:firstDatabaseIndex-1]
+
+    for i in dataBases:
+        if(i not in loadedDatabases):
+            print("Target database not loaded")
+            return None
+    newDatabase = {}
+    
+    #chat gpt
+    db1 = dataBases[0]
+    db2 = dataBases[1]
+    
+    if(commandType == "Union"):
+         result_db = {
+        "title": f"{db1['title']}Union{db2['title']}",
+        "labels": db1["labels"],
+        "entries": db1["entries"] + [entry for entry in db2["entries"] if entry not in db1["entries"]]
+        }
+    elif(commandType == "Intersect"):
+         # Combine titles and labels
+        result_db = {
+            "title": f"{db1['title']}Intersect{db2['title']}",
+            "labels": db1["labels"],
+            "entries": [entry for entry in db1["entries"] if entry in db2["entries"]]
+        }
+    elif(commandType == "Difference"):
+        # Combine titles and labels
+        result_db = {
+            "title": f"{db1['title']}Difference{db2['title']}",
+            "labels": db1["labels"],
+            "entries": [entry for entry in db1["entries"] if entry not in db2["entries"]]
+        }
+    else:
+        print("Unknown type of set")
+        return None    
+    
+    return result_db
+    
+def join(args):   
+    #join EmployeeID(Employees,Managers)")
+    
+    firstDatabaseIndex = indexOf(args, '(')+1
+    dataBases = args[firstDatabaseIndex:-1].split(",")
+    targetLabel = args[0:firstDatabaseIndex-1]
+   
+    if(len(dataBases) != 2):
+        print("Specify 2 databases")
+        return None
+    
+    for i in dataBases:
+        if(i not in loadedDatabases):
+            print("Target database not loaded" + i)
+            return None
+    newDatabase = {}
+
+    return database_inner_join(loadedDatabases[dataBases[0]], loadedDatabases[dataBases[1]], targetLabel)
+    
+#chat gpt
+def database_inner_join(db1, db2, join_column):
+    # Find the index of the join column in both databases
+    index_db1 = db1["labels"].index(join_column)
+    index_db2 = db2["labels"].index(join_column)
+
+    # Combine titles and labels
+    result_db = {
+        "title": f"{db1['title']} Join {db2['title']} on {join_column}",
+        "labels": db1["labels"] + [label for label in db2["labels"] if label != join_column],
+        "entries": []
+    }
+
+    # Perform the join
+    for entry1 in db1["entries"]:
+        for entry2 in db2["entries"]:
+            if entry1[index_db1] == entry2[index_db2]:
+                result_db["entries"].append(entry1 + [value for j, value in enumerate(entry2) if j != index_db2])
+
+    return result_db
 
 def project(args):
-    pass
+    #displays the column specified
+    #project Age(Employees)
+    index = indexOf(args, "(")
+    targetLabel = args[0:index]
+    
+    targetDatabase = args[index+1:-1]
+    
+    if(targetDatabase not in loadedDatabases):
+        print("Target database not loaded " + targetDatabase)
+        return None
+    
+    database = loadedDatabases[targetDatabase]
+    if(targetLabel not in database["labels"]):
+        print("label not found in target database "+ targetLabel)
+        return None
+    
+    labelIndex = database["labels"].index(targetLabel)
+    copyDatabase = database.copy()
+    copyDatabase["labels"] = [targetLabel]
+    entries = []
+    for entry in database["entries"]:
+        entries += entry[labelIndex]
+    copyDatabase["entries"] = entries
+    return copyDatabase
+
     
 if __name__ == "__main__":
-    #if len(sys.argv) != 2:
-    #    print("Usage: python code.py <database file path>")
-    #    sys.exit(1)
+
+    #order of commands is separated by space
+
+
+
+
+    oneAtAtime
+
+
+    def oneAtAtime():
+
+        # Get the file path from the command-line argument
+        #file_path = sys.argv[1]
+        file_path = "Data/Employees.txt"
         
-    # Get the file path from the command-line argument
-    #file_path = sys.argv[1]
-    file_path = "Data/test1.txt"
-    
-    # Example usage
-    newdatabase = loadDatabase(file_path)
-    loadedDatabases[newdatabase["title"]] = newdatabase
+        # Example usage
+        newdatabase = loadDatabase(file_path)
+        loadedDatabases[newdatabase["title"]] = newdatabase
 
-    print("Loaded database: ")
-    #printDatabase(database)
-
-
-    while True:
-        print("Please input a command")
-        print("available commands: select, join, project, set, list")
-
-        #operations
-        inputString = input()
-
-        data = inputString.split()
-        command = data[0]
-        args = data[1:]
+        file_path = "Data/Managers.txt"
         
+        # Example usage
+        newdatabase = loadDatabase(file_path)
+        loadedDatabases[newdatabase["title"]] = newdatabase
 
-        if(command == "load"):
-            newdatabase = loadDatabase(file_path)
-            if(newdatabase == None):
-                print("File not found")
-            loadedDatabases[newdatabase["title"]] = newdatabase
-        elif(command == "list"):
-            print("loaded databases:")
-            for item in loadedDatabases:
-                print(item)            
-        elif(command == "select"):
-            result = select(args[0])
-            if(result == None):
+        file_path = "Data/Salaries.txt"
+        
+        # Example usage
+        newdatabase = loadDatabase(file_path)
+        loadedDatabases[newdatabase["title"]] = newdatabase
+
+
+        print("Loaded database: ")
+        #printDatabase(database)
+
+
+        while True:
+            print("\nPlease input a command")
+            print("available commands: select, join, project, set, list, help")
+
+            #operations
+            inputString = input()
+            if(not inputString):
                 continue
-            else:
-                printDatabase(result)
+
+            data = inputString.split()
+            command = data[0]
+            args = inputString[len(command):].strip()
             
-        elif(command == "join"):
-            join(args)  
-        elif(command == "project"):
-            project(args)
-        elif(command == "set"):
-            databaseSet(args)
+            if(command == "load"):
+                newdatabase = loadDatabase(file_path)
+                if(newdatabase == None):
+                    print("File not found")
+                loadedDatabases[newdatabase["title"]] = newdatabase
+            
+            elif(command == "examples" or command == "help"):
+                print("Examples of commands: ")
+                print("")
+                print("list")
+                print("load filepath")
+                print("")
+                print("select Age<30(Employees)")
+                print("project Age(Employes)")
+                print("")
+                print("set Union(Employees,Managers)")
+                print("set Intersect(Employees,Managers)")
+                print("set Difference(Employees,Managers)")
+                print("")
+                print("join EmployeeID(Employees,Managers)")  
+                
+            elif(command == "list"):
+                print("Loaded databases:")
+                for item in loadedDatabases:
+                    print(item)            
+                    
+                    
+            elif(command == "select"):
+                result = select(args)
+                if(result == None):
+                    continue
+                else:
+                    printDatabase(result)
+                
+            elif(command == "join"):
+                result = join(args)
+                if(result == None):
+                    continue
+                else:
+                    printDatabase(result)
+                
+            elif(command == "project"):
+                result = project(args)
+                if(result == None):
+                    continue
+                else:
+                    printDatabase(result)
+                    
+                    
+            elif(command == "set"):
+                result = databaseSet(args)
+                if(result == None):
+                    continue
+                else:
+                    printDatabase(result)
